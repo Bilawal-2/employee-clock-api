4614cd2f83a4e9b01ce868f59de313a18b71378a
Muhammad Bilawal <bilawal225@gmail.com>
2022-09-26 01:32:53 +0500

    edit mongoserver link.

--- db/mongoosedb.js
+++ db/mongoosedb.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dbBilawal:dbFMI@employeeclockclustor.txbf3.mongodb.net/employee-clock', 
-{useNewUrlParser: true, useUnifiedTopology: true});
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://demo.net/employee-clock", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
   // we're connected!
-  console.log("we re connected")
+  console.log("we re connected");
 });
